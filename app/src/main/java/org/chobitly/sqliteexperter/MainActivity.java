7f6d90369540e5945c3dbd540a3435e0a980b5d0
chobitly <chobitly@gmail.com>
2015-04-26 03:15:02 +0800

    Finish ActionBar & Navigation Tab on the whole.

---
+++ app/src/main/java/org/chobitly/sqliteexperter/MainActivity.java
@@ -0,0 +1,138 @@
+package org.chobitly.sqliteexperter;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
+
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+
+    ExportFragment mExportFragment;
+    HistoryFragment mHistoryFragment;
+    CollectionPagerAdapter mCollectionPagerAdapter;
+
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_HOME);
+
+        mExportFragment = ExportFragment.newInstance(null, null);
+        mHistoryFragment = HistoryFragment.newInstance(null, null);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mCollectionPagerAdapter = new CollectionPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mCollectionPagerAdapter);
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        for (int i = 0; i < mCollectionPagerAdapter.getCount(); ++i) {
+            actionBar.addTab(actionBar.newTab().setText(mCollectionPagerAdapter.getPageTitle(i))
+                    .setTabListener(this));
+        }
+
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // When swiping between pages, select the
+                // corresponding tab.
+                getActionBar().setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // do nothing
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // do nothing
+    }
+
+    // Since this is an object collection, use a FragmentStatePagerAdapter,
+    // and NOT a FragmentPagerAdapter.
+    public class CollectionPagerAdapter extends FragmentStatePagerAdapter {
+        public CollectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                default:
+                case 0:
+                    return mExportFragment;
+                case 1:
+                    return mHistoryFragment;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                default:
+                case 0:
+                    return getString(R.string.fragment_export);
+                case 1:
+                    return getString(R.string.fragment_history);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_export, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
